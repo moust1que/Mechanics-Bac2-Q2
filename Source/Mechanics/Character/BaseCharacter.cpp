f5801c131dbb34fe9ce40434063cac82bb5910a2
moust1que <emiliensalpeteur@gmail.com>
2025-04-27 20:26:10 +0200

    Charcter setup

--- Source/Mechanics/Character/BaseCharacter.cpp
+++ Source/Mechanics/Character/BaseCharacter.cpp
@@ -1,10 +1,29 @@
 #include "BaseCharacter.h"
 #include "../Ability/AbilityBase.h"
 #include "InputAction.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
-// Called when the game starts or when spawned
+ABaseCharacter::ABaseCharacter() {
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+    SpringArmComponent->SetupAttachment(RootComponent);
+    SpringArmComponent->TargetArmLength = 2000.f;
+    SpringArmComponent->bDoCollisionTest = false;
+    SpringArmComponent->bUsePawnControlRotation = false;
+    SpringArmComponent->bInheritPitch = false;
+    SpringArmComponent->bInheritRoll = false;
+    SpringArmComponent->bInheritYaw = false;
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+    CameraComponent->FieldOfView = 60.0f;
+    CameraComponent->bUsePawnControlRotation = false;
+}
+
 void ABaseCharacter::BeginPlay() {
 	Super::BeginPlay();
+
+    SpringArmComponent->SetRelativeRotation(FRotator(-45.0f, 0.f, 0.f));
 
     for(const auto& AbilityPair : AbilityMap) {
         if(AbilityPair.Value) {
@@ -24,4 +43,31 @@ void ABaseCharacter::ActivateAbility(FName AbilitySlot) {
     if(UAbilityBase** Ability = InstantiatedAbilities.Find(AbilitySlot)) {
         (*Ability)->ActivateAbility(this);
     }
+}
+
+void ABaseCharacter::Ability1() {
+    UE_LOG(LogTemp, Warning, TEXT("Ability1"));
+}
+
+void ABaseCharacter::Ability2() {
+    UE_LOG(LogTemp, Warning, TEXT("Ability2"));
+}
+
+void ABaseCharacter::Ability3() {
+    UE_LOG(LogTemp, Warning, TEXT("Ability3"));
+}
+
+void ABaseCharacter::Ability4() {    
+    UE_LOG(LogTemp, Warning, TEXT("Ability4"));
+}
+
+void ABaseCharacter::ZoomCamera(const FInputActionInstance& Instance) {
+    float axisValue = Instance.GetValue().Get<float>();
+
+    if(FMath::Abs(axisValue) <= 0.3f) return;
+
+    float newLength = SpringArmComponent->TargetArmLength - axisValue * CameraZoomSpeed * GetWorld()->GetDeltaSeconds();
+    newLength = FMath::Clamp(newLength, CameraZoomMin, CameraZoomMax);
+
+    SpringArmComponent->TargetArmLength = newLength;
 }
--- Source/Mechanics/Character/BaseCharacter.h
+++ Source/Mechanics/Character/BaseCharacter.h
@@ -7,12 +7,16 @@
 // Forward declaration
 class UAbilityBase;
 struct FInputActionInstance;
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class MECHANICS_API ABaseCharacter : public ACharacter {
 	GENERATED_BODY()
 
     public:
+        ABaseCharacter();
+
         UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Abilities) TMap<FName, TSubclassOf<UAbilityBase>> AbilityMap; // "Q", "W", "E", "R"
 
         UPROPERTY() TMap<FName, UAbilityBase*> InstantiatedAbilities;
@@ -21,7 +25,21 @@ class MECHANICS_API ABaseCharacter : public ACharacter {
 
         void ActivateAbility(FName AbilitySlot); // "Q", "W", etc.
 
+        UPROPERTY(EditAnywhere, Category = Camera) float CameraZoomSpeed = 100.0f;
+        UPROPERTY(EditAnywhere, Category = Camera) float CameraZoomMin = 1200.0f;
+        UPROPERTY(EditAnywhere, Category = Camera) float CameraZoomMax = 2500.0f;
+
     protected:
         // Called when the game starts or when spawned
         virtual void BeginPlay() override;
+
+    private:
+        USpringArmComponent* SpringArmComponent;
+        UCameraComponent* CameraComponent;
+
+        UFUNCTION() void Ability1();
+        UFUNCTION() void Ability2();
+        UFUNCTION() void Ability3();
+        UFUNCTION() void Ability4();
+        UFUNCTION() void ZoomCamera(const FInputActionInstance& Instance);
 };
